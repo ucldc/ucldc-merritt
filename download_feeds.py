2eb090b406a341c3b225e59779d0046cf76efc6c
Barbara Hui <barbara.hui@ucop.edu>
2017-08-31 16:51:42 -0700

    Add download_feeds.py script

---
+++ download_feeds.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+import boto3
+
+bucketname = 'static.ucldc.cdlib.org'
+prefix = 'merritt/'
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucketname)
+
+for obj in bucket.objects.filter(Prefix=prefix):
+    if obj.key.endswith('.atom'):
+        print "downloading {}".format(obj.key)
+        filename = obj.key.split('/')[1] 
+        filepath = './feeds_current/{}'.format(filename)
+        print "local filepath: {}".format(filepath)
+        s3.Bucket('static.ucldc.cdlib.org').download_file(obj.key, filepath)
